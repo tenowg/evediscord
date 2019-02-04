30394f9097ff1e0b938acbc9d1b4a3fa320757e9
tenowg <tenowg@gmail.com>
2019-02-04 06:29:46 +0000

    permissions

--- src/services/BotAuthService.php
+++ src/services/BotAuthService.php
@@ -6,6 +6,6 @@ class BotAuthService {
     public function authRedirectURL() {
         $client_id = config('evediscord.client_id');
         $client_permissions = config('evediscord.permissions');
-        return config('evediscord.end_point') . '/oauth2/authorize?client_id=' . $client_id . '&scope=bot&permissions=' . $client_id;
+        return config('evediscord.end_point') . '/oauth2/authorize?client_id=' . $client_id . '&scope=bot&permissions=' . $client_permissions;
     }
 }
